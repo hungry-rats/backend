a4460fc2c69f76ae4d57ecd5e2ce7d2fc35686c1
DylanDrechsel <DrechselDylan@gmail.com>
2021-03-29 11:26:15 -0400

    cleaned up an error

--- db/seeds.js
+++ db/seeds.js
@@ -1,15 +1,10 @@
 const mongoose = require('./connection');
-
 const Recipes = require('../models/Recipes');
-
 const Data = require('./seeds.json');
 
 Recipes.deleteMany({})
-
 	.then(() => Recipes.insertMany(Data))
-
 	.catch(console.error)
-
 	.finally(() => {
 		process.exit;
 	});
--- models/Recipes.js
+++ models/Recipes.js
@@ -2,23 +2,15 @@ const mongoose = require('../db/connection');
 
 const Recipes = new mongoose.Schema({
 	title: String,
-
 	author: String,
-
 	inspiredBy: String,
-
 	allergies: String,
-
 	likes: Number,
-
 	image: String,
-
 	ingredients: [
 		{
 			name: String,
-
 			ingredients: [String],
-
 			directions: [String],
 		},
 	],
