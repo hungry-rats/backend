d32ada1baad3b7f8bcce258bc603fe04761cd69f
DylanDrechsel <DrechselDylan@gmail.com>
2021-03-29 13:31:14 -0400

    User Schema in test

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+const mongoose = require('../db/connection');
+const Recipes = require('../models/Recipes')
+import bcrypt from 'bcryptjs'
+
+const Users = new mongoose.Schema({
+    username: {type:String, unique:true},
+    email: {type:String, required:true, unique:true},
+    password: {type:String}, 
+    recipes: [Recipes]
+})
+
+Users.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+Users.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const UsersModel = mongoose.model('Users', Users)
+
+module.exports = UsersModel
