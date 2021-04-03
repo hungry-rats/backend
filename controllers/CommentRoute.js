e420ba9a241400e42084e801002fc3077d3dc2f3
GrandZizi <josmarbm@gmail.com>
2021-04-03 18:57:06 -0400

    Comment routes with auth

--- controllers/CommentRoute.js
+++ controllers/CommentRoute.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Comment = require('../models/Comments').CommentsModel;
 const Recipe = require('../models/Recipes');
 const mongoose = require('mongoose')
-const toId = mongoose.Types.ObjectId
 const { requireToken, createUserToken } = require('../middleware/auth');
 const {handleValidateOwnership} = require('../middleware/custom_errors')
 
@@ -16,29 +15,6 @@ router.get('/:recipeId/comments', (req, res, next) => {
 
 });
 
-//POST creates comment on recipe
-// router.post('/:userId/:recipeId/comments/create', (req, res, next) => {
-// 	Comment.create(req.body)
-// 		.then((comment) => {
-// 			userObjectId = toId(req.params.userId);
-// 			comment.author = userObjectId;
-
-// 			return comment.save()
-// 		})
-// 		.then(comment => {
-// 			console.log(comment);
-// 			Recipe.findById({_id:req.params.recipeId})
-// 				.then((recipe) => {
-// 					recipe.comments.push(comment);
-// 					return recipe.save()
-// 				})
-// 				.then(added => {
-// 					res.json(added)
-// 				})
-// 				.catch(next)
-// 		})
-// 		.catch(next)
-// });
 
 // USER POST COMMENT TO RECIPE
 // FINAL PRODUCT
@@ -64,6 +40,7 @@ router.post('/:recipeId/comments/create', requireToken, (req, res, next) => {
 		});})
 
 //PUT updates
+//FINAL PRODUCT
 router.put('/:recipeId/comments/:commentId/edit',requireToken, (req, res, next) => {
 	Comment.findById(req.params.commentId)
 		.then((comment) => {
@@ -78,7 +55,6 @@ router.put('/:recipeId/comments/:commentId/edit',requireToken, (req, res, next) 
 				.then( (recipe) => {
 					recipe.comments.forEach(async(rComment) => {
 						if (rComment._id.toString() == comment._id.toString()) {
-							console.log(rComment);
 							rComment.post = comment.post;
 							await recipe.save();
 						}
@@ -90,6 +66,7 @@ router.put('/:recipeId/comments/:commentId/edit',requireToken, (req, res, next) 
 
 
 
+	//Old version for demonstration 
 
 	// Comment.findByIdAndUpdate({ _id: req.params.id }).then((comment) => {
 	// 	console.log(comment);
@@ -104,20 +81,46 @@ router.put('/:recipeId/comments/:commentId/edit',requireToken, (req, res, next) 
 });
 
 //DELETE
-router.delete('/comments/:id/delete', (req, res, next) => {
-	Comment.findByIdAndDelete({_id:req.params.id})
-		.then((comment) => {
-			console.log(comment);
-			Recipe.findOne({'comments._id':req.params.id})
-				.then(recipe => {
-					recipe.comments.id(req.params.id).remove()
-					return recipe.save()
-				})
-				.then(() => {
-					res.sendStatus(204)
+//FINAL PRODUCT
+router.delete('/:recipeId/comments/:commentId/delete',requireToken, (req, res, next) => {
+	Comment.findById(req.params.commentId)
+		.then(comment => {
+			return handleValidateOwnership(req,comment)
+		})
+		.then(() => {
+			Comment.findById(req.params.commentId)
+				.then(comment => {
+					Recipe.findById(req.params.recipeId)
+						.then(recipe =>{
+							recipe.comments.forEach(async(rComment,index) => {
+
+								if(rComment._id.toString() == comment._id.toString()){
+									recipe.comments.splice(index,1)
+									await recipe.save()
+									
+								}
+								res.status(201)
+							})
+						})
 				})
 		})
-		.catch(next)
+
+
+	//old version
+
+	// Comment.findByIdAndDelete({_id:req.params.id})
+	// 	.then((comment) => {
+	// 		console.log(comment);
+	// 		Recipe.findOne({'comments._id':req.params.id})
+	// 			.then(recipe => {
+	// 				recipe.comments.id(req.params.id).remove()
+	// 				return recipe.save()
+	// 			})
+	// 			.then(() => {
+	// 				res.sendStatus(204)
+	// 			})
+	// 	})
+	// 	.catch(next)
 	});
 
 
